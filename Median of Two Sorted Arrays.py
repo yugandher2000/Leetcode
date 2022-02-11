f63d4d5a39dc0dcdbc22e6a0b68c295bc7314e34
balasaraswathi yugandher <61632113+yugandher2000@users.noreply.github.com>
2022-02-11 09:22:05 +0530

    Update Median of Two Sorted Arrays.py

--- Median of Two Sorted Arrays.py
+++ Median of Two Sorted Arrays.py
@@ -9,3 +9,13 @@ class Solution:
             return ((nums[int(i/2)]+nums[int((i/2)-1)])/2)  
         else:
             return nums[int(i/2)]
+        
+        
+#solution-2
+import numpy as np
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        nums3 = nums1+nums2
+        nums3.sort()
+        median = np.median(nums3)
+        return median
